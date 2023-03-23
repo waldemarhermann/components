34c1a63a4856e35a2baa161760de27233323ef5a
waldemarhermann <hermann.waldemar1995@gmail.com>
2023-03-23 11:20:31 +0100

    changed directory

--- src/components/Button.js
+++ src/components/Button.js
@@ -9,9 +9,10 @@ const Button = ({
     danger,
     outline,
     rounded,
+    ...rest
 }) => {
 
-    const classes = className('px-3 py-1.5 border', {
+    const classes = className(rest.className, 'flex items-center px-3 py-1.5 border', {
         'border-blue-500 bg-blue-500 text-white': primary,
         'border-gray-900 bg-gray-900 text-white': secondary,
         'border-green-500 bg-green-500 text-white': succes,
@@ -26,7 +27,7 @@ const Button = ({
         'text-red-500': outline && danger,
     });
 
-    return <button className={classes}>{children}</button>
+    return <button {...rest} className={classes}>{children}</button>
 }
 
 export default Button;
