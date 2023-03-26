ae837c9b651c89baca4675a94f7dd737d8387240
waldemarhermann <hermann.waldemar1995@gmail.com>
2023-03-26 14:08:10 +0200

    add state-system, event-listener and handler, style

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,14 +1,32 @@
+import { useState } from "react"
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
+
 const Accordion = ({ items }) => {
-    const renderedItems = items.map((item) => {
+
+    const [expandedIndex, setExpandedIndex] = useState(-1);
+
+    const handleClick = (nextIndex) => {
+        (expandedIndex === nextIndex) ? setExpandedIndex(-1) : setExpandedIndex(nextIndex);
+    };
+
+    const renderedItems = items.map((item, index) => {
+        const isExpanded = expandedIndex === index;
+        const icon = <span className="text-xl">
+            {isExpanded ? <GoChevronDown /> : <GoChevronLeft />}
+        </span>
+
         return (
             <div key={item.id}>
-                {item.label}
-                {item.content}
+                <div className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer" onClick={() => handleClick(index)} >
+                    {item.label}
+                    {icon}
+                </div>
+                {isExpanded && <div className="border-b p-5">{item.content}</div>}
             </div>
         )
     })
 
-    return <div>{renderedItems}</div>
+    return <div className="border-x border-t rounded">{renderedItems}</div>
 
 }
 
