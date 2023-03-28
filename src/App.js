99528703515c923ea9c236f6175a0775eff03865
waldemarhermann <hermann.waldemar1995@gmail.com>
2023-03-28 14:31:43 +0200

    add new array of objects for Dropdown component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import Accordion from "./components/Accordion";
+import { useState } from "react";
+import Dropdown from "./pages/Dropdown";
 
 const App = () => {
-    const items = [
-        {
-            id: '123rdfwe',
-            label: 'Überschrift 1',
-            content: 'Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-        },
-        {
-            id: '1232135rfwdf',
-            label: 'Überschrift 2',
-            content: 'Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-        },
-        {
-            id: 'sdfgwst345634',
-            label: 'Überschrift 3',
-            content: 'Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-        }
+    const [selection, setSelection] = useState(null);
+
+    const options = [
+        { label: 'Red', value: 'red' },
+        { label: 'Green', value: 'green' },
+        { label: 'Blue', value: 'blue' }
     ]
-    return <Accordion items={items} />
+
+    return (
+        <div>
+            <Dropdown options={options} />
+        </div>
+    )
 }
 
 export default App;
