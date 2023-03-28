00a50e830f2ce82d619e343e709001921645ccd7
waldemarhermann <hermann.waldemar1995@gmail.com>
2023-03-28 14:32:26 +0200

    create new component Dropdown

---
+++ src/pages/Dropdown.js
@@ -0,0 +1,31 @@
+import { useState } from "react"
+
+
+
+const Dropdown = ({ options }) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const handleClick = () => {
+        setIsOpen(!isOpen);
+    }
+    const handleOptionClick = (option) => {
+        setIsOpen(false);
+        console.log(option.label);
+    }
+
+    const renderedOptions = options.map((option) => {
+        return (
+            <div onClick={() => handleOptionClick(option)} key={option.value}>
+                {option.label}
+            </div>
+        )
+    })
+
+    return (
+        <div>
+            <div onClick={handleClick} >Select...</div>
+            {isOpen && <div>{renderedOptions}</div>}
+        </div>
+    )
+}
+
+export default Dropdown;
