3def5cebcda4632907bf73a82838dc4c753ad6ac
waldemarhermann <hermann.waldemar1995@gmail.com>
2023-03-28 14:32:10 +0200

    create new component

---
+++ src/pages/AccordionPage.js
@@ -0,0 +1,25 @@
+import Accordion from "../components/Accordion";
+
+const AccordionPage = () => {
+    const items = [
+        {
+            id: '123rdfwe',
+            label: 'Überschrift 1',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        },
+        {
+            id: '1232135rfwdf',
+            label: 'Überschrift 2',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        },
+        {
+            id: 'sdfgwst345634',
+            label: 'Überschrift 3',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        }
+    ]
+
+    return <Accordion items={items} />
+}
+
+export default AccordionPage;
